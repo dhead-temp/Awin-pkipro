c8e9af6e2ff6419e3ecef2f02a5f689a756ff298
digiheadway <99395515+digiheadway@users.noreply.github.com>
2023-03-22 13:12:43 +0530

    folder upload

---
+++ php/post_data.php
@@ -0,0 +1,42 @@
+<?php
+// Establish database connection
+$dbhost = "localhost";
+$dbuser = "u240376517_awin";
+$dbpass = "IGmad;gqZ#j5";
+$dbname = "u240376517_awin";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get user input
+$name = $_POST['name'];
+$mobile = $_POST['mobile'];
+$city = $_POST['city'];
+
+// Check if the user already exists
+$check_query = "SELECT * FROM users WHERE mobile='$mobile'";
+$check_result = mysqli_query($conn, $check_query);
+if (mysqli_num_rows($check_result) > 0) {
+    echo "You have already submitted your details!";
+} else {
+    // Insert user data into database
+    $insert_query = "INSERT INTO users (name, mobile, city) VALUES ('$name', '$mobile', '$city')";
+    if (mysqli_query($conn, $insert_query)) {
+        // Set cookies for the user
+        setcookie('name', $name, time() + (86400 * 30), "/");
+        setcookie('mobile', $mobile, time() + (86400 * 30), "/");
+        setcookie('city', $city, time() + (86400 * 30), "/");
+        
+        // Redirect to another page
+        header('Location: ../share2.php');
+    } else {
+        echo "Error: " . $insert_query . "<br>" . mysqli_error($conn);
+    }
+}
+
+// Close database connection
+mysqli_close($conn);
+?>
