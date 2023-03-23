dcac2078d67da3fa6e07616280c2c1afbaf2823d
digiheadway <99395515+digiheadway@users.noreply.github.com>
2023-03-23 15:18:15 +0530

    temp fix

--- php/post_data.php
+++ php/post_data.php
@@ -1,10 +1,10 @@
 <?php
 
-// Get user input
-$name = $conn->real_escape_string($_POST['name']);
-$mobile = $conn->real_escape_string($_POST['mobile']);
-$city = $conn->real_escape_string($_POST['city']);
-$p = $conn->real_escape_string($_POST['p']);
+// // Get user input
+$name = $_POST['name'];
+$mobile = $_POST['mobile'];
+$city = $_POST['city'];
+$p = $_POST['p'];
 
 // Set cookies for the user
 setcookie('name', $name, time() + (86400 * 30), "/");
@@ -12,8 +12,4 @@ setcookie('mobile', $mobile, time() + (86400 * 30), "/");
 setcookie('city', $city, time() + (86400 * 30), "/");
 setcookie('p', $p, time() + (86400 * 30), "/");
 
-// header('Location: ../share2.php');
-
-?>
-
-
+header('Location: ../share2.php');
