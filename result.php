97e3b53aeb15609a0931958d916ba8b292776538
digiheadway <99395515+digiheadway@users.noreply.github.com>
2023-03-23 19:08:40 +0530

    title update

--- result.php
+++ result.php
@@ -15,7 +15,7 @@ $fail = ($sum < 2);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Awin</title>
+    <title>Result</title>
 
     <!-- ==== link === -->
     <link rel="stylesheet" href="assets/style.css?v2.10">
