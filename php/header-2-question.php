53479a7f828a28a23bf3029a3dd8b2bc8e5d066e
digiheadway <99395515+digiheadway@users.noreply.github.com>
2023-03-27 09:39:41 +0530

    headline updated

--- php/header-2-question.php
+++ php/header-2-question.php
@@ -2,6 +2,6 @@
                 <div class="heroMoney_img">
                     <img src="assets/img/money.png?version=0.0.3" alt="money">
                 </div>
-                <div class="rightHero_textbox">Answer & Win 4100 Rs</div>
+                <div class="rightHero_textbox">Answer & Get 4100 Rs Cash</div>
                 <a href="#winnerslist" class="winList" onclick="gtag('event', 'see_winners_list');" >See Winners List</a>
             </div>
