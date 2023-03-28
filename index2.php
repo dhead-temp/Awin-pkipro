8beac3c1b0c9b85cf1e87b11028b06a665a11fee
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2023-03-28 17:31:33 +0530

    check border color

--- index2.php
+++ index2.php
@@ -56,7 +56,7 @@ include('php/head.php');
         }
         .main-container-box{
             margin-top: 7px;
-            border-top: 1px;
+            border-top: 1px solid #e4e4e4;
             
         }
         .qution-box{
